1d51399dfb61dbc0d05fb6f208c40b3e535fadf8
Oleksandr H <pamail08@gmail.com>
2023-09-26 11:34:05 +0200

    lesson2

---
+++ src/main/java/de/telran/lesson2/TypesData.java
@@ -0,0 +1,18 @@
+package de.telran.lesson2;
+
+public class TypesData {
+    public static void main(String[] args) {
+        int myInt;
+        myInt = 25;
+        System.out.println(myInt);
+        myInt = 13;
+
+        byte myByte = 18; //zelochislenniy Tip
+        System.out.println(myByte);
+        float myFloat = (float) 3.14555555555F;
+        System.out.println(myFloat);
+        System.out.println("myByte = "+myByte);
+        System.out.println("myFloat = "+myFloat);
+        System.out.println("myFloat = "+myFloat);
+    }
+}
